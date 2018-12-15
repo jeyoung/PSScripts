724852b052f7d68610fa07e5cd8bc08b2212593e
Eddy Young <jeyoung@priscimon.com>
2018-12-15 09:59:45 +0400

    Prevent name conflict during rename

--- Renumber-Items.ps1
+++ Renumber-Items.ps1
@@ -10,14 +10,28 @@ param (
 
 $counter = 0
 $items = Get-ChildItem -Path .
-Write-Host "Renaming $($items.Length) file(s)..."
-$items | ForEach-Object {
+$items | Sort-Object -Property 'LastWriteTime' | ForEach-Object {
     $counter = $counter + 1
     $name = "$Prefix` -` $($counter.ToString('000#'))$($_.Extension)"
-    Write-Host "$_.Name --> $name"
+    $tempPrefix = [string](New-Guid)
+    $tempName = "$tempPrefix`_$name"
+    if (-Not $Staging) {
+        Rename-Item -Path $_.Name -NewName $tempName
+    }
+    Write-Verbose "$($_.Name) --> $tempName"
+}
+
+$counter = 0
+$items = Get-ChildItem -Path .
+Write-Host "Renaming $($items.Length) file(s)..."
+$items | Sort-Object -Property 'LastWriteTime' | ForEach-Object {
+    $counter = $counter + 1
+    $name = "$Prefix` -` $($counter.ToString('000#'))$($_.Extension)"
+    Write-Verbose "$($_.Name) --> $tempName"
     if (-Not $Staging) {
         Rename-Item -Path $_.Name -NewName $name
     }
+    Write-Host "$($_.Name) --> $name"
 }
 Write-Host ''
 Write-Host "$counter file(s) renamed. Done."
